b2c7c2a5bd7bd1a3cff443c780e20284ee03c505
Aayan-Ismail <mohammed143ismail@gmail.com>
2026-09-19 17:57:54 +0100

    Add files via upload

---
+++ stardust.py
@@ -0,0 +1,38 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 1000
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((0,0,0))
+    
+    pygame.draw.line(screen, (255,255,255),(500,100),(675,600), 10)
+    pygame.draw.line(screen, (255,255,255),(500,100),(325,600), 10)
+  
+    pygame.draw.line(screen, (255,255,255),(675,600),(550,550), 10)
+    pygame.draw.line(screen, (255,255,255),(325,600),(450,550), 10)
+
+    pygame.draw.line(screen, (255,255,255),(550,550),(500,500), 10)
+    pygame.draw.line(screen, (255,255,255),(450,550),(500,500), 10)
+    
+    pygame.draw.polygon(screen, (255,255,255), [(250,300), (426.5,300), (395,401)])
+    pygame.draw.polygon(screen, (255,255,255), [(250,300), (426.5,300), (395,401)])
+
+    
+    pygame.draw.circle(screen, (220, 230, 255), (500,350), 50)
+    pygame.draw.circle(screen, (255,255,255), (500,350), 40)
+    pygame.display.update()
+
+
+pygame.quit()
+
