2aa1f67b6008d89560308ea816715e7d8987f3d3
Aayan-Ismail <mohammed143ismail@gmail.com>
2026-09-26 17:08:26 +0100

    Add files via upload

--- stardust.py
+++ stardust.py
@@ -26,9 +26,8 @@ while running:
     pygame.draw.line(screen, (255,255,255),(450,550),(500,500), 10)
     
     pygame.draw.polygon(screen, (255,255,255), [(250,300), (426.5,300), (395,401)])
-    pygame.draw.polygon(screen, (255,255,255), [(250,300), (426.5,300), (395,401)])
+    pygame.draw.polygon(screen, (255,255,255), [(570,300), (740.5,300), (605,401)])
 
-    
     pygame.draw.circle(screen, (220, 230, 255), (500,350), 50)
     pygame.draw.circle(screen, (255,255,255), (500,350), 40)
     pygame.display.update()
